47a66e2efc1118209f8d3444080bb584eebb63e6
palliddreams <krause.h@gmail.com>
2022-04-01 22:35:17 -0600

    Attempting Blog Views
    Why it doesn't work I don't know.

---
+++ BlogTemplate.App/Models/BlogModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BlogTemplate.App.Models
+{
+    public class BlogModel
+    {
+        public int BlogID { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public int UserID { get; set; }
+        public string Content { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ BlogTemplate.App/Models/CommentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BlogTemplate.App.Models
+{
+    public class CommentModel
+    {
+
+        public int UserCommentID { get; set; }
+        public int? UserID { get; set; }
+        public int BlogID { get; set; }
+        public string Comment { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+    }
+}
